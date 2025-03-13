f4ead202ffdd5923af04756e5c7e4e052c6f82cc
Programozás Karrier <lapispeter69@gmail.com>
2025-03-13 08:59:55 +0100

    Kesz

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,33 @@ namespace CarRent2.Models
 {
     internal class Customer
     {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public List<Car> RentedCars { get; set; } = new List<Car>();
+
+        public void RentCar(Car car)
+        {
+            if (car.isAvailable)
+            {
+
+                car.isAvailable = false;
+                RentedCars.Add(car);
+            }
+        }
+        public void ReturnCar(Car car)
+        {
+            if (RentedCars.Contains(car))
+            {
+                car.isAvailable = true;
+                RentedCars.Remove(car);
+                Console.WriteLine($"{car.Title} sikeresen visszaadva!");
+            }
+            else
+            {
+                Console.WriteLine($"{car.Title} nem volt kikölcsönözve!");
+            }
+        }
+
+
     }
 }
---
+++ Models/RentalService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent2.Models
+{
+    internal class RentalService
+    {
+        public List<Car> Cars { get; set; } = new List<Car> { };
+        public List<Customer> Customers { get; set; } = new List<Customer> { };
+
+        public void AddCustomer(Customer customer)
+        {
+            Customers.Add(customer);
+        }
+    }
+}
